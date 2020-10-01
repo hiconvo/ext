abf0d234529f20f7be210b4faa229d490282534b
Alexander Richey <alexander.richey@gmail.com>
2020-10-01 13:27:13 -0700

    revise approach to auth

--- src/options.js
+++ src/options.js
@@ -1,17 +1,39 @@
 const message = document.getElementById("message");
 const clearButton = document.getElementById("clear");
 const authenticateButton = document.getElementById("authenticate");
+const formContent = document.getElementById("formcontent");
 
 function showLogin() {
-  message.innerText = "Please log in to save your links to Convo.";
   clearButton.style.display = "none";
+  message.innerText =
+    "Before you can use this extension, you'll have to login.";
   authenticateButton.style.display = "inline-block";
+  formContent.innerHTML = `
+    <div>
+      <p class="helptext">If you do not have a password associated with your account, you can create one on Convo's <a href="https://app.convo.events/settings" target="_blank">settings page</a>.</p>
+    </div>
+
+    <div>
+      <p class="error" id="errorcontainer"></p>
+    </div>
+
+    <label>
+      <span>Email</span>
+      <input type="email" name="email" id="email" required autofocus>
+    </label>
+    <label>
+      <span>Password</span>
+      <input type="password" name="password" id="password" required>
+    </label>
+  `;
 }
 
 function showLogout() {
-  message.innerHTML = "You are logged into Convo. &#x1f60a";
+  message.innerHTML =
+    "You are logged into Convo. &#x1f60a <br /><br /> You can now close this window and save links as you browse by clicking the Convo icon in the upper toolbar.";
   clearButton.style.display = "inline-block";
   authenticateButton.style.display = "none";
+  formContent.innerHTML = "";
 }
 
 chrome.storage.local.get(["token"], values => {
@@ -28,22 +50,42 @@ clearButton.addEventListener("click", () => {
   });
 });
 
-authenticateButton.addEventListener("click", () => {
-  const redirectURL = encodeURIComponent(chrome.identity.getRedirectURL());
-  const authURL = `https://oauth.convo.events/?redirect_uri=${redirectURL}`;
+authenticateButton.addEventListener("click", e => {
+  e.preventDefault();
 
-  return chrome.identity.launchWebAuthFlow(
-    {
-      interactive: true,
-      url: authURL
-    },
-    redirect => {
-      const parsed = new URL(redirect);
-      const encodedToken = parsed.searchParams.get("token");
-      const token = decodeURIComponent(encodedToken);
-      chrome.storage.local.set({ token }, () => {
-        showLogout();
+  authenticateButton.disabled = true;
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  fetch("https://api.convo.events/users/auth", {
+    headers: new Headers({
+      "Content-Type": "application/json"
+    }),
+    method: "POST",
+    mode: "cors",
+    body: JSON.stringify({
+      email: email,
+      password: password
+    })
+  }).then(response => {
+    response
+      .json()
+      .then(decoded => {
+        if (response.status == 200) {
+          chrome.storage.local.set({ token: decoded.token }, () => {
+            showLogout();
+          });
+        } else {
+          authenticateButton.disabled = false;
+          document.getElementById("errorcontainer").innerText =
+            decoded.message || "That didn't work. Please try again.";
+        }
+      })
+      .catch(() => {
+        authenticateButton.disabled = false;
+        document.getElementById("errorcontainer").innerText =
+          "Something went wrong.";
       });
-    }
-  );
+  });
 });
